60b4964c0d08d87d71fad94ffb9c9a6e22b606aa
Eduardo <eduardobullonvera@gmail.com>
2025-06-03 17:52:10 -0500

    Added full Spring Boot project with CRUD functionality for 'Alumno' and other configurations (JPA, AOP, H2 console)

---
+++ PRJ_CRUD_SPRING_BOOT/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO cursos (nombre,creditos) VALUES('Calculo', 5);
+INSERT INTO cursos (nombre,creditos) VALUES('Fisica', 5);
+INSERT INTO cursos (nombre,creditos) VALUES('Programacion', 5);
+
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, correo, telefono) VALUES ('Eduardo', 'Bullon', '2000-05-15', 'eduardo.Bullon@example.com', '987764321');
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, correo, telefono) VALUES ('Raul', 'Prado', '1998-03-22', 'raul.prado@example.com', '3456678934');
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, correo, telefono) VALUES ('Nicolas', 'Castro', '1995-11-30', 'nicolas.castro@example.com', '987654321');
